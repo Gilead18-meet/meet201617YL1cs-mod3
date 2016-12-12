aa4b8d802860434a976d42228fc459f9e6bf938a
none <none>
2016-12-12 19:47:48 +0200

    student

---
+++ student.py
@@ -0,0 +1,16 @@
+class student:
+
+    def __init__(name, hometown, age, height, favorite ice-cream flavor):
+
+        self.name=name
+        self.hometown=hometown
+        self.age=age
+        self.height=height
+        self.favorite ice-cream flavor=favorite ice-cream flavor
+
+    def print_all(self):
+        print(self.name+"from"+self.hometown+self.age+"years old"+self.height+"meter"+"i like"+self.favorite ice-cream flavor)
+
+    def get_girrafe_gap(self):
+        return 500-self.height
+    
